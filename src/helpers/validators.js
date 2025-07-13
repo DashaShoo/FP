268453ec496e3834abc02a5a804827a46a9904a2
DashaShoo <daryashubabko@gmail.com>
2025-07-13 23:31:12 +0300

    first task done

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -15,39 +15,56 @@
 
 
 // prop - Returns a function that when supplied an object returns the indicated property of that object, if it exists.
-// R.propEq(propName, value) - Возвращает предикат, проверяющий, что у объекта свойство propName имеет значение value.
-// R.propEq(key, value, obj) - Это функция из Ramda, проверяющая, что в объекте obj у свойства key — значение value.
-// R.allPass([pred1, pred2, ...]) - Создаёт предикат, который вернёт true, только если все предикаты из массива вернут true.
-// R.anyPass([pred1, pred2, ...]) - Создаёт предикат, который вернёт true, если хотя бы один из предикатов вернёт true.
-// R.values(obj) - Возвращает массив значений объекта.
-// R.filter(predicate, array) - Фильтрует элементы массива по предикату.
-// R.countBy(fn, list) - Создаёт объект, в котором ключи — это результат применения fn к каждому элементу, а значения — количество таких элементов.
-// R.toPairs(obj) - Преобразует объект в массив пар [ключ, значение].
-// R.length(array) - Возвращает длину массива.
-// R.pipe(f1, f2, ..., fn) - Создаёт композицию функций слева направо: результат f1 передаётся в f2, и т.д.
-// R.equals(a)(b) или R.equals(a, b) - Проверяет на строгую равенство (===). Можно каррировать.
-// R.gte(a, b) и R.gte(R.__, 2) - Проверка "больше или равно". Вторая форма использует R.__ — это placeholder (заглушка), позволяющая каррировать аргументы в любом порядке.
-// R.complement(fn) - Создаёт функцию, противоположную по логике: !fn.
-// R.all(predicate, array) - Проверяет, что все элементы массива удовлетворяют предикату.
-// R.identity(x) - Просто возвращает x — полезна как функция по умолчанию.
+// propEq(propName, value) - Возвращает предикат, проверяющий, что у объекта свойство propName имеет значение value.
+// propEq(key, value, obj) - Это функция из Ramda, проверяющая, что в объекте obj у свойства key — значение value.
+// allPass([pred1, pred2, ...]) - Создаёт предикат, который вернёт true, только если все предикаты из массива вернут true.
+// anyPass([pred1, pred2, ...]) - Создаёт предикат, который вернёт true, если хотя бы один из предикатов вернёт true.
+// values(obj) - Возвращает массив значений объекта.
+// filter(predicate, array) - Фильтрует элементы массива по предикату.
+// countBy(fn, list) - Создаёт объект, в котором ключи — это результат применения fn к каждому элементу, а значения — количество таких элементов.
+// toPairs(obj) - Преобразует объект в массив пар [ключ, значение].
+// length(array) - Возвращает длину массива.
+// pipe(f1, f2, ..., fn) - Создаёт композицию функций слева направо: результат f1 передаётся в f2, и т.д.
+// equals(a)(b) или equals(a, b) - Проверяет на строгую равенство (===). Можно каррировать.
+// gte(a, b) и gte(__, 2) - Проверка "больше или равно". Вторая форма использует __ — это placeholder (заглушка), позволяющая каррировать аргументы в любом порядке.
+// complement(fn) - Создаёт функцию, противоположную по логике: !fn.
+// all(predicate, array) - Проверяет, что все элементы массива удовлетворяют предикату.
+// identity(x) - Просто возвращает x — полезна как функция по умолчанию.
 
 //Что такое каррирование?
 // Функция, принимающая несколько аргументов, превращается в цепочку функций, каждая из которых принимает по одному аргументу:
 
 // // до каррирования
-// (color, figure, obj) => R.propEq(figure, color, obj)
+// (color, figure, obj) => propEq(figure, color, obj)
 
 // // после
 // isFigureColor('green')('circle')(obj)
 
 
+import {
+  allPass,
+  pipe,
+  propEq,
+  __,
+  any,
+  curry,
+  equals,
+  filter,
+  length,
+  values,
+  prop,
+  converge,
+  identity,
+  countBy,
+  complement,
+  gte,
+} from 'ramda';
 
-import * as R from 'ramda';
 
 
 // Базовый предикат: фигура нужного цвета
-const isFigureColor = R.curry((color, figure, obj) =>
-  R.propEq(figure, color, obj)
+const isFigureColor = curry((color, figure, obj) =>
+  propEq(figure, color, obj)
 );
 
 // Частично применённые предикаты
@@ -57,17 +74,17 @@ const isFigureBlue = isFigureColor('blue');
 const isFigureWhite = isFigureColor('white');
 const isFigureOrange = isFigureColor('orange');
 
-const isFigureNotRed = R.complement(isFigureRed);
-const isFigureNotWhite = R.complement(isFigureWhite);
+const isFigureNotRed = complement(isFigureRed);
+const isFigureNotWhite = complement(isFigureWhite);
 
 //Подсчёт количества фигур определённого цвета
-const countColor = R.curry((color, obj) =>
-  R.pipe(R.values, R.filter(R.equals(color)), R.length)(obj)
+const countColor = curry((color, obj) =>
+  pipe(values, filter(equals(color)), length)(obj)
 );
 
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = R.allPass([
+export const validateFieldN1 = allPass([
   isFigureRed('star'),
   isFigureGreen('square'),
   isFigureWhite('triangle'),
@@ -75,44 +92,42 @@ export const validateFieldN1 = R.allPass([
 ]);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = R.pipe(
-    countColor('green'), R.gte(R.__, 2)
+export const validateFieldN2 = pipe(
+    countColor('green'), gte(__, 2)
 );
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = R.converge(R.equals, [countColor('red'), countColor('blue')]);
+export const validateFieldN3 = converge(equals, [countColor('red'), countColor('blue')]);
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = R.allPass([
+export const validateFieldN4 = allPass([
   isFigureRed('star'),
   isFigureOrange('square'),
   isFigureBlue('circle'),
 ]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = (obj) => {
-  return R.pipe(
-    R.values,
-    R.filter(R.complement(R.equals('white'))),
-    R.countBy(R.identity),
-    R.values,
-    R.any(R.gte(R.__, 3))
-  )(obj);
-};
+export const validateFieldN5 = pipe(
+    values,
+    filter(complement(equals('white'))),
+    countBy(identity),
+    values,
+    any(gte(__, 3))
+);
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = R.allPass([
-    isFigureGreen('triangle'), R.pipe(countColor('green'), R.equals(2)), R.pipe(countColor('red'), R.equals(1))
+export const validateFieldN6 = allPass([
+    isFigureGreen('triangle'), pipe(countColor('green'), equals(2)), pipe(countColor('red'), equals(1))
 ]);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = R.converge(R.equals, [countColor('orange'), R.pipe( R.values,  R.length)]);
+export const validateFieldN7 = converge(equals, [countColor('orange'), pipe( values,  length)]);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = R.allPass([isFigureNotRed('star'), isFigureNotWhite('star')]);
+export const validateFieldN8 = allPass([isFigureNotRed('star'), isFigureNotWhite('star')]);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 =  R.converge(R.equals, [countColor('green'), R.pipe( R.values,  R.length)]);
+export const validateFieldN9 =  converge(equals, [countColor('green'), pipe( values,  length)]);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = R.allPass([isFigureNotWhite('triangle'), isFigureNotWhite('square'), R.converge(R.equals, [R.prop('triangle'), R.prop('square')])])
+export const validateFieldN10 = allPass([isFigureNotWhite('triangle'), isFigureNotWhite('square'), converge(equals, [prop('triangle'), prop('square')])])
